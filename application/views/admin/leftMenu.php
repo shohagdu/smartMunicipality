b2f218d7d19ad35d9334b3fdff1b2a635bdb6af3
sirajul83 <sirajulislam8354@gmail.com>
2022-06-05 10:59:12 +0600

    some menu hide

--- application/views/admin/leftMenu.php
+++ application/views/admin/leftMenu.php
@@ -66,7 +66,7 @@
 								?>
                             </ul>
                         </li>
-                        <li class="accordion">
+                        <!-- <li class="accordion">
                             <a href="javascript:void(0);"><i class="glyphicon glyphicon-tags"></i><span> &nbsp;খাদ্য বান্ধব কর্মসূচি</span> &nbsp;<i class="glyphicon glyphicon-chevron-down"></i></a>
                             <ul class="nav nav-pills nav-stacked">
                                 <li><a href="FoodController" >আবেদনকারীগন </a></li>
@@ -77,29 +77,29 @@
                                 <li><a  href="DailyReports/foodApplicantReport" target="_blank">রিপোর্ট আবেদনকারীগন </a></li>
                                 <li><a href="DailyReports/foodCollectionReport" target="_blank">রিপোর্ট খাদ্য সংগ্রহ</a></li>
                             </ul>
-                        </li>
-                         <li class="accordion">
+                        </li> -->
+                         <!-- <li class="accordion">
                             <a href="javascript:void(0);"><i class="glyphicon glyphicon-tags"></i><span> &nbsp;ভিজিডি খাদ্য বিতরণ</span> &nbsp;<i class="glyphicon glyphicon-chevron-down"></i></a>
                             <ul class="nav nav-pills nav-stacked">
                                 <li><a href="VgdController" >সদস্য গন </a></li>
-                                <!--
+                                
                                 <li><a href="VgdController/distributesRecord"> বিতরণের তথ্য সমূহ </a></li>
-                                -->
+                                
                                 <li><a href="VgdController/setupInfo">সেটিংস সমূহ </a></li>
-                                <!--
+                                
                                 <li><a  href="DailyReports/vgdApplicantReport" target="_blank">সদস্যেদের রিপোর্ট </a></li>
-                                 -->
+                                 
                                 <li><a href="DailyReports/vgdDistributesReport" target="_blank">বিতরণের রিপোর্ট</a></li>
 
                             </ul>
-                        </li>
+                        </li> -->
 
-						<li>
+						<!-- <li>
 							<a class="ajax-link" href="Admin/mamlaNotice">
 								<i class="glyphicon glyphicon-tags"></i> &nbsp;
 								<span>মামলার নোটিশ তৈরি </span>
 							</a> 
-						</li>
+						</li> -->
 						
 						<li class="nav-header hidden-md">Collection Section</li>
 						
